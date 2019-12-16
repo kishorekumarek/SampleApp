0e309ae9064c44330777e027d82435702ca9ffa3
rameez <rameez@pegasus.ae>
2019-12-16 13:06:51 +0400

    Initial commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Rameez on 12/16/19.
+//  Copyright © 2019 Rameez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

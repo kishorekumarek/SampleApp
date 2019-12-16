eb82261045694946c3caa1d58cc97d98cf16b227
Kishore Kumar <kishorekanaga@Kishores-MacBook-Pro.local>
2019-12-16 16:27:57 +0400

    Removed print statement in VC

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("View Did load")
+        
         // commit and push develop
         // add a comment
     }
